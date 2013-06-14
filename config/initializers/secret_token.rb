bcef23e9e52701750874c7b5e708169782488331
Michael Ries <michael@riesd.com>
2013-06-13 22:54:38 -0600

    vanilla rails 3.2.13 application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ComicCollector::Application.config.secret_token = '437159b7a16104f6f0e0daaa389c780286460dbb7778061ee9e2cf841ff22b9ebe1584e05d84de0b39076532911128c5b89a29b6c46d7537599f28d81b7ab7e8'
